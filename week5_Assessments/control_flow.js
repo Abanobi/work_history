d9639e006fcff45a07e46f9c095da9df81cad18b
Abanobi <you@example.com>
2018-01-28 13:54:15 +0100

    Assessments

---
+++ week5_Assessments/control_flow.js
@@ -0,0 +1,86 @@
+/**
+  In this assessment, you will use your knowledge of javascript control flows
+  to complete the functions below.
+*/
+
+// 1. Complete the `isEven` function
+// it returns `true` if the num is even
+// and `false` if it is not
+
+function isEven(num) {
+  isEven num;
+  	if (num === isEven)
+  		return true;
+  	else
+  		return false;
+  }
+
+// 2. The function Number.isNaN(num) will return true if num is not a number
+// or false if it is.
+// Use isNan to extend your `isEven` function by including an `else if` statement
+// that checks whether a num is a number and
+// console.log => "That is not a number", if it is not a number
+
+function isNaN(num){
+isEvenv2 num;
+isNaN num;
+}if(isNaN !== num, && isEven === num);{
+    return true;
+ }else {
+    return false;{
+   console.log("That is not a number");
+}
+
+
+// 3. Complete the switch statment in the `getLunchOrder` with the following
+// case conditions:
+// > "sandwich" - console.log => "Sure thing! One sandwich, coming up."
+// > "soup" - console.log => "Got it! Tomato's my favorite."
+// > "salad" - console.log => "Sounds good! How about a caesar salad?"
+// > "pie" - console.log => "Pie's not a meal!"
+// > "default" - console.log => "How does a sandwich sound?"
+
+function getLunchOrder(lunchOrder) {
+  switch(lunchOrder);{
+    case 'sandwich':
+    console.log('Sure thing! one sandwich, coming up');
+    break;
+    case 'soup':
+    console.log('Got it! Tomatos my favorite');
+    break;
+    case  'salad':
+    console.log('sounds good! how about a caesar salad?');
+    break;
+    case'pie':
+    console.log('Pies not a meal ');
+    break;
+    default:
+    console.log('how does a sandwich sound?');
+  }
+
+
+// 4. Complete the `shouldINap` function
+// it returns `true` if mood is equal to "tired" or "sleepy"
+// and `false` if it is not
+
+function shouldINap(mood){
+    shouldINap mood;
+  }if( mood === tired && mood === sleepy);{
+    return true;
+   }else{
+     return false;
+   }
+
+
+
+// 5. Extend the `shouldINap` function
+// to include an `else if` statement that checks if mood is equal to "energetic"
+// then console.log => "Go use that energy and learn more javascript ;)"
+
+function shouldINap(mood) {
+  shouldINap mood;
+    if(mood === energetic);
+     console.log("Go use that energy and learn more javascript");
+   }else}
+     console.log('you need energy to learn javascript');
+   }
