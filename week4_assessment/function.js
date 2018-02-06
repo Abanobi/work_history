8d9a87b65a01fa3f5590d31d0a8fad763ea23e28
Abanobi <you@example.com>
2018-02-06 12:00:38 +0100

    Assessments

---
+++ week4_assessment/function.js
@@ -0,0 +1,90 @@
+/**
+  In this assessment, you will be creating new Javascript functions.
+  Make sure to read the directions to build the functions carefully.
+  You should use the below patterns to write your functions:
+  ```
+
+
+
+  OR
+
+  const addSquares = (a,b) => {
+ const square = x => x*x;
+ return square(a) + square(b);
+};
+
+  ```
+*/
+
+// 1. write a function that takes a number as a parameter and returns it as a
+// string
+function returToString(number){
+return  2 .ToString();
+  "" + 2;
+console.log (.ToString + '2');
+}
+returToString(number);
+
+// 2. write a function that takes a number as a parameter and returns that subtracted by 1.
+
+function subtractByOne(number){
+return number * 1;
+  console.log(number * 1);
+}
+subtractByOne(4);
+
+// 3. write a function that takes a number as a parameter and returns that
+// number added by 1
+function addOneToNumber(number1,number2){
+  return number1 + number2 + 1;
+  console.log(number1 + number2 + 1);
+}
+addOneToNumber(4,4);
+
+// 4. write a function that takes two number as its parameters and returns then
+// addition of those two numbers
+
+function addTwoToNumber(number,number){
+  return number1 + number1;
+  console.log(number1 + number1);
+
+}
+  addTwoToNumber(5,7);
+
+// 5. write a function that takes two number as its parameters and returns then
+// squared of those two numbers
+function squareOfNumber(number1,number2) {
+  return number * number * 2;
+  console.log(number * number * 2);
+}
+squareOfNumber(4,4);
+
+// 6. write a function that takes a number and returns true if the number is odd
+// and false if it is not
+function isOdd(number){
+if(number === Odd  || num === odd){
+  return(true);
+}else{
+  return (false)
+}
+}
+isOdd(number);
+
+// 7. write a function that takes two strings as its parameters and returns the
+// combination of those two strings
+
+function combineTwoStrings('Abanobi','kingsley'){
+return 'Abanobi' + 'kingsley';
+console.log('my name is' + combineTwoStrings +'.' );
+}
+combineTwoStrings('Abanobi', 'kingsley');
+
+// 8. write a function that takes the radius of a circle a parameter and returns
+// the Area of the circle.
+// For this exercise π (pie) can equal `3.14`
+
+function areaOfCircle(pie){
+.radius(areaOfCircle(d){return Math.sqrt(pie);})
+console.log(pie * 3.14);
+}
+  rediusOfACircle (pie)
